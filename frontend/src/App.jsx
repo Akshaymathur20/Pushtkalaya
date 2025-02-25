581d99f012ea38610e6502ca0f06e0a3a9e9f08d
Akshay Mathur <akshaymathur242@gmail.com>
2025-02-25 23:54:55 +0530

    first

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Pushkalaya Project</h1>
+      <p>Get a accesss of large books</p>
+    </div>
+  )
+}
+
+export default App
